71e7494eb0d0a7c7da636e7e5eea752383d50d5c
MarcelGolubarschDG <marcel.golubarsch@datagroup.de>
2025-03-28 20:49:09 +0100

    push

--- IAMCalculator/src/app/home/home.component.ts
+++ IAMCalculator/src/app/home/home.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ItemService } from '../services/item.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  items: any[] = [];
+  newItemName: string = '';
 
+  constructor(private itemService: ItemService) {}
+
+  ngOnInit(): void {
+    this.loadItems();
+  }
+
+  loadItems(): void {
+    this.itemService.getItems().subscribe(data => {
+      this.items = data;
+    });
+  }
+
+  addItem(): void {
+    if (this.newItemName.trim() === '') return;
+
+    this.itemService.addItem(this.newItemName).subscribe(item => {
+      this.items.push(item);
+      this.newItemName = ''; // Eingabefeld leeren
+    });
+  }
 }
